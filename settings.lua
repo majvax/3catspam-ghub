2e3ecd08b0fbee6c22f163e90301972569a08c28
majvax <77504686+majvax@users.noreply.github.com>
2024-04-30 23:54:13 +0200

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,25 @@
+return {
+    cat_spam_key = 5,
+    shotgun_save = 4,
+    refresh_key = 2,
+
+    weapon = {
+        heavy    = {key = 0x07, delay = 500},
+        sniper   = {key = 0x06, delay = 450},
+        special  = {key = 0x05, delay = 450},
+        melee    = {key = 0x04, delay = 400},
+        backup   = {key = 0x03, delay = 450},
+        primary  = {key = 0x02, delay = 500}
+    },
+    
+    order = {
+        "sniper",
+        "special",
+        "backup",
+        "primary",
+        "sniper",
+        "special",
+        "backup",
+        "heavy"
+    }
+}
